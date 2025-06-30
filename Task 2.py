b51c7ea99896baf4ce82bb242f76a9d45a2d1a56
kristina-bilenko <kristinabilenkok@gmail.com>
2025-06-30 23:31:11 +0300

    .

---
+++ Task 2.py
@@ -0,0 +1,11 @@
+import re
+from typing import Callable
+def generator_numbers(text):
+    pattern = r'\d+\.\d+|\d+'
+    for match in re.findall(pattern,text):
+        yield float(match)
+
+def sum_profit(text: str, func: Callable):
+    return sum(func(text))
+
+
