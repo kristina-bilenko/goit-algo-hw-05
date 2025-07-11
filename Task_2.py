711e12684d9c42e31bb5a458ee6176caf538754a
kristina-bilenko <kristinabilenkok@gmail.com>
2025-07-11 15:55:03 +0300

    .

--- Task_2.py
+++ Task_2.py
@@ -1,7 +1,7 @@
 import re
 from typing import Callable
 def generator_numbers(text):
-    pattern = r'\d+\.\d+|\d+'
+    pattern = r' \d+\.\d+ | \d+ '
     for match in re.findall(pattern,text):
         yield float(match)
 
